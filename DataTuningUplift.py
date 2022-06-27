6e6d69b8a69896618a1cd332a5ba6afe648debfc
Nick Schmeiter <nickschmeiter98@gmail.com>
2022-06-27 16:10:25 +0200

    classification and data tuning organized

--- DataTuningUplift.py
+++ DataTuningUplift.py
@@ -1,11 +1,8 @@
+import numpy as np
+
+# remove nans and outliers
 def woNaNOutliersU(ds):
     ds2 = ds.dropna(axis=0, how="any")
-
-    plt.figure
-    plt.title("Box Plot")
-    sns.boxplot(ds2["uplift"])
-    plt.show()
-    print(ds2.shape)
 
     out = []
     def Zscore_outlier(df):
@@ -21,21 +18,10 @@ def woNaNOutliersU(ds):
 
     for i in out:
         ds2.drop(index=next(iter(ds2[ds2['Uplift'] == i].index)), inplace=True)
-    print(ds2.shape)
-    plt.title("Box Plot after outlier removing")
-    sns.boxplot(ds2["uplift"])
-    plt.show()
     return ds2
 
-
+# remove outliers and replace nans with mean
 def woOutliersMeanU(ds):
-
-    plt.figure
-    plt.title("Box Plot")
-    sns.boxplot(ds["uplift"])
-    plt.show()
-    print(ds.shape)
-
     # outlier kill with Z-score
     out = []
 
@@ -52,10 +38,6 @@ def woOutliersMeanU(ds):
 
     for i in out:
         ds.drop(index=next(iter(ds[ds['uplift'] == i].index)), inplace=True)
-    print(ds.shape)
-    plt.title("Box Plot after outlier removing")
-    sns.boxplot(ds["uplift"])
-    plt.show()
 
     ds4 = ds.fillna(ds.mean())
 
